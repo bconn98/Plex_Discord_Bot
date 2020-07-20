3a92b164180b484e1b7e71fdab32fa5d199d216d
Jarod Godlewski <jarodgodlewski@live.com>
2020-07-20 17:48:01 -0400

    Basic on_message
    Basic discord bot to reply to a simple command. Set-up work for all other commands.

---
+++ discordbot.py
@@ -0,0 +1,45 @@
+# bot.py
+import os
+
+import discord
+from dotenv import load_dotenv
+
+load_dotenv()
+TOKEN = os.getenv('DISCORD_TOKEN')
+GUILD = os.getenv('DISCORD_GUILD')
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    guild = discord.utils.get(client.guilds, name=GUILD)
+    print(
+        f'{client.user} is connected to the following guild:\n'
+        f'{guild.name}(id: {guild.id})'
+    )
+
+@client.event
+async def on_member_join(member):
+    await member.create_dm()
+    await member.dm_channel.send(
+        f'Hi {member.name}, welcome to my Discord server!'
+    )
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content == '!bog':
+        response = "BOG CHURCH"
+        await message.channel.send(response)
+
+@client.event
+async def on_error(event, *args, **kwargs):
+    with open('err.log', 'a') as f:
+        if event == 'on_message':
+            f.write(f'Unhandled message: {args[0]}\n')
+        else:
+            raise
+        
+client.run(TOKEN)
