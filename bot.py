54ea544f0f91294f2a1c85e44f7bdf1b3711a14e
Jarod Godlewski <jarodgodlewski@live.com>
2020-07-20 20:02:25 -0400

    Update bot.py
    Added Admin only !dq command to remove things from the add-to-server queue

--- bot.py
+++ bot.py
@@ -5,6 +5,7 @@ date: 7/20/2020
 """
 
 import os
+
 import discord
 from dotenv import load_dotenv
 from discord.ext import commands
@@ -22,7 +23,7 @@ PLEX_TOKEN = os.getenv('PLEX_TOKEN')
 bot = commands.Bot(command_prefix='!')
 plex_server = PlexServer(PLEX_URL, PLEX_TOKEN)
 
-queue = Queue()
+list = []
 
 @bot.command(name='bog', help='Responds with a bog moment')
 async def bog(ctx):
@@ -31,19 +32,21 @@ async def bog(ctx):
 
 @bot.command(name='queue', help='Queues a Movie/TV Show to be downloaded if it is not already on the server or queue.\nIf multi-worded use double quotes surrounding it.')
 async def botqueue(ctx, name_of_media):
-    if add_to_queue(plex_server, queue, name_of_media):
+    if add_to_list(plex_server, list, name_of_media):
         await ctx.send('Media has been added to download queue. \nCurrent Queue: \n'  + display_queue())
     else:
         await ctx.send('Media is already present in download queue or server. \nCurrent Queue: \n'  + display_queue())
 
+@bot.command(name='dq', help='Removes media from queue (Admin Only)')
+@commands.has_permissions(administrator=True)
+async def remove(ctx, name_of_media):
+    list.remove(name_of_media)
+    await ctx.send(name_of_media + ' removed from Queue.\nCurrent Queue: ' + display_queue())
+
 def display_queue():
-    queue_size = queue.qsize()
     result = ''
-
-    for i in range(queue_size):
-        queue_front = queue.get()
-        queue.put(queue_front)
-        result += queue_front + ' '
-
+    for a, b in enumerate(list, 1):
+        result += '{}. {}\n'.format(a, b)
     return result
+
 bot.run(TOKEN)
